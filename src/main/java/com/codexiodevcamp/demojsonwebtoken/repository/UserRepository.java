1996d6dc6e9aa6758b510dbe2d7c7cd3850c2962
Nike <nagrozdanov@gmail.com>
2019-06-27 11:57:10 +0300

    replace project

--- src/main/java/com/codexiodevcamp/demojsonwebtoken/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codexiodevcamp.demojsonwebtoken.repository;
-
-import com.codexiodevcamp.demojsonwebtoken.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-}
